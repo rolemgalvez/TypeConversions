1c2692f7ce783317a2bd6a6f7b1c2c88c03ff5cc
Rolem Galvez <rolemgalvez@gmail.com>
2022-11-07 16:00:51 +0800

    Add project files.

---
+++ Convertables/Program.cs
@@ -0,0 +1,36 @@
+﻿// Welcome message
+Console.WriteLine("Convertables");
+Console.WriteLine("------------");
+Console.WriteLine();
+
+// string to int using Parse
+string textValue = "123";
+int numberValue = int.Parse(textValue);
+Console.WriteLine($"Text Value                  : {textValue}");
+Console.WriteLine($"Number Value                : {numberValue}");
+Console.WriteLine();
+
+// int to double using casting
+int intValue = 456;
+double doubleValue = (int)intValue;
+Console.WriteLine($"Integer Value               : {intValue}");
+Console.WriteLine($"Double Value                : {doubleValue}");
+Console.WriteLine();
+
+// double to decimal using casting
+double anotherDouble = 67.89;
+decimal decimalValue = (decimal)anotherDouble;
+Console.WriteLine($"Another Double Value        : {anotherDouble}");
+Console.WriteLine($"Decimal Value               : {decimalValue}");
+Console.WriteLine();
+
+// string to int using TryParse
+Console.Write("Given me some real number   : ");
+int.TryParse(Console.ReadLine(), out int realNumber);
+Console.WriteLine($"It Is Converted Into        : {realNumber}");
+Console.WriteLine();
+
+Console.Write("Give me now worded number   : ");
+int.TryParse(Console.ReadLine(), out int worderedNumber);
+Console.WriteLine($"Now, It Is Converted To     : {worderedNumber}");
+Console.ReadLine();
---
+++ Represent/Program.cs
@@ -0,0 +1,8 @@
+﻿//Welcome message
+Console.WriteLine("Represent");
+Console.WriteLine("---------");
+Console.WriteLine();
+
+//string to int
+string priceTag = "442";
+int price = int.Parse(priceTag);
